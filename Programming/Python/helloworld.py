a59e7b97889216e3a2796d19fc95e5c393ce65a1
Alumanda Shakankale <alumandas.sil@gmail.com>
2019-05-05 19:58:17 +0200

    git init git add

--- Programming/Python/helloworld.py
+++ Programming/Python/helloworld.py
@@ -1,4 +1,8 @@
 #Welcome to Python
+#Don't have python installed
+#Try these on https://colab.research.google.com/notebooks/welcome.ipynb#scrollTo=OZsL21O2o6Sy
+#Go to  File and then New Python 3 notebook
+
 
 print("Hello, World!")
 
@@ -6,3 +10,6 @@ print("Hello, World!")
 #You try
 
 Print("Your message")
+
+
+
